555ce361638bc19765650e6c5bc171ec3927fac9
Stefan-Tasev <stefan123tasev@gmail.com>
2024-06-09 20:55:58 +0300

    finded solution of the problem 04 - Reveal Words

--- entry-course/11-string-processing - exercise/04-revealWords.js
+++ entry-course/11-string-processing - exercise/04-revealWords.js
@@ -1,12 +1,16 @@
 function revealWords(someWords, someString) {
-    let words = someWords.split(', ');
-    let sentenceArray = someString.split(' ');
 
-    for (let word of sentenceArray) {
-        if (word.includes('*')) {
-            
+    let words = someWords.split(', ');
+    let pattern = /\*+/g;
+    let sentenceArray = someString.matchAll(pattern);
+    
+    for (let wordMatch of sentenceArray) {
+        let word = words.find(w => w.length === wordMatch[0].length);
+        if (word) {
+            someString = someString.replace(wordMatch[0], word);
         }
     }
+    console.log(someString);
 }
 
 revealWords('great', 'JavaScript* is ***** programming language');
