893ae8a6290f08bdd0716013115e2b69d3cfdb24
Stefan-Tasev <stefan123tasev@gmail.com>
2024-07-06 15:14:26 +0300

    modify the style of the paragraph text through javascript code

---
+++ HTML-and-CSS/DOM-Exercise/task-01/script.js
@@ -0,0 +1,11 @@
+function restyle() {
+
+    const paragraphElement = document.getElementById('text');
+
+    paragraphElement.style.color = 'green';
+    paragraphElement.style.backgroundColor = 'aqua';
+    paragraphElement.style.display = 'inline-block';
+    paragraphElement.style.fontSize = '24px';
+    paragraphElement.style.padding = '10px'
+    
+}
