7d6ccc530c9a8bb609c3ae6182b2d53c0dff91f3
Stefan-Tasev <stefan123tasev@gmail.com>
2024-09-02 13:36:41 +0300

    connect db with server

--- bookApp/server.js
+++ bookApp/server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const path = require('path');
+
+const connectDb = require('./db');
+const bookRoutes = require('./controllers/book-controller')
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () =>{
+app.use('/books', bookRoutes);
+
+app.set('views', path.join(__dirname, 'views'))
+
+/* const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}...`)
-}).on('error', err => console.log(`SERVER ERROR\n${err}`));
+}).on('error', err => console.log(`SERVER ERROR\n${err}`)); */
+
+connectDb()
+    .then(data => {
+        console.log('DB connection succeeded!');
+
+        const PORT = process.env.PORT || 5002;
+        app.listen(PORT, () => {
+            console.log(`Server listen at port ${PORT}...`)
+        }).on('error', err => console.log(`SERVER ERROR\n${err}`));
+
+    }).catch(err => console.log(`Error in connecting DB\n${err}`));
