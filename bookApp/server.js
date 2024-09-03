53f9cf6aca5f098b3f89d6b500d3d764c9ceeb94
Stefan-Tasev <stefan123tasev@gmail.com>
2024-09-03 17:35:35 +0300

    extend server functionality

--- bookApp/server.js
+++ bookApp/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const path = require('path');
 const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const connectDb = require('./db');
-const bookRoutes = require('./controllers/book-controller')
+const bookRoutes = require('./controllers/book-controller');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.use('/books', bookRoutes);
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', engine({
     extname: 'hbs',//handlebars
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    //defaultLayout: 'mainLayout.hbs',
 }));
 app.set('view engine', '.hbs')
 
