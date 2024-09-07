81e98e503c2823bc5d7c998f473356884daf312d
Stefan-Tasev <stefan123tasev@gmail.com>
2024-09-07 11:21:18 +0300

    create final-exam project for EURO 2024

---
+++ final-exam/src/Home.jsx
@@ -0,0 +1,25 @@
+import Footer from "./Footer";
+
+export default function Home() {
+    return (
+        <>
+            <header>
+                <div>
+                    <h2> UEFA EURO 2024</h2>
+                </div>
+                <div>
+                    <ul>
+                        <li>Matches</li>
+                        <li>Group Stage</li>
+                        <li>News</li>
+                        <li>Statistics</li>
+                    </ul>
+                </div>
+            </header>
+            <main>
+
+            </main>
+            <Footer/>
+        </>
+    )
+}
