66966746ee9aba84ea7247c8986acefceb048bbc
Stefan-Tasev <stefan123tasev@gmail.com>
2024-09-07 12:22:25 +0300

    create header component with some styles

---
+++ final-exam/src/Header.jsx
@@ -0,0 +1,21 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <>
+            <header className="home-header">
+                <div>
+                    <h2> UEFA EURO 2024</h2>
+                </div>
+                <div>
+                    <ul>
+                        <li>Matches</li>
+                        <li>Group Stage</li>
+                        <li>News</li>
+                        <li>Statistics</li>
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
+}
