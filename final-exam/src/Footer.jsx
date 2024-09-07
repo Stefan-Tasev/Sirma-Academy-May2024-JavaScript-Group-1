1a07a67e033125130ac6ae018749aa24b6532318
Stefan-Tasev <stefan123tasev@gmail.com>
2024-09-07 12:21:44 +0300

    create  static footer component with some styles

--- final-exam/src/Footer.jsx
+++ final-exam/src/Footer.jsx
@@ -1,8 +1,12 @@
+import "./Footer.css"
+
 export default function Footer() {
     return (
         <>
-            <p>© 1998-2024 UEFA. All rights reserved.</p>
-            <p>The UEFA word, the UEFA logo and all marks related to UEFA competitions, are protected by trademarks and/or copyright of UEFA. No use for commercial purposes may be made of such trademarks. Use of UEFA.com signifies your agreement to the Terms and Conditions and Privacy Policy.</p>
+            <footer className="home-footer">
+                <p>© 1998-2024 UEFA. All rights reserved.</p>
+                <p>The UEFA word, the UEFA logo and all marks related to UEFA competitions, are protected by trademarks and/or copyright of UEFA. No use for commercial purposes may be made of such trademarks. Use of UEFA.com signifies your agreement to the Terms and Conditions and Privacy Policy.</p>
+            </footer>
         </>
     )
 }
